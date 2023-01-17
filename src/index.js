e649f37a6e4a823d8942c4671f0503b2ba9e934e
Jakhongir <zhaxongir.sirozhiddinov@gmail.com>
2023-01-18 00:07:45 +0500

    Add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 !== 0) return false;
+
+  const pairs = Object.fromEntries(bracketsConfig);
+  const reversePairs = {};
+  const stack = [];
+
+  for (const [left, right] of Object.entries(pairs)) {
+    reversePairs[right] = left;
+  }
+
+  for (const bracket of str) {
+    const lastInStack = stack[stack.length - 1];
+    if (pairs[bracket]) {
+      if (stack.length > 0 && lastInStack === pairs[bracket]) {
+        stack.pop();
+      } else {
+        stack.push(bracket);
+      }
+    } else if (stack.length > 0 && lastInStack === reversePairs[bracket]) {
+      stack.pop();
+    }
+  }
+
+  return stack.length === 0;
+};
